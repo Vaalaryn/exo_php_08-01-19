6f413ce8aa62e096f329f783f7700f3977ab0a2e
brice <brice.bitot@neuf.fr>
2019-01-08 09:09:40 +0100

    ajout de personne

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?php 
-	$personne[] = [
-		'nom' => '',
-		'prenom' => '',
-		'age' => '',
-	]
+<?php
+	$line = readline('Voulez vous ajouter ou voir les personnes referencer(a pour ajouter et v pour voir et q pour quitter)');
+	$nbpersonne = 0;
+	while ($line!= 'q') {
+		if ($line == 'a'){
+			$personne[$nbpersonne] = [
+				'nom' => readline('Entrez son Nom : '),
+				'prenom' => readline('Entrez son Prenom : '),
+				'age' => readline('Entrez son Age : '),
+				];
+			$nbpersonne++;
+			prompt('personne bien ajouter')
+		}
+		
+		
+		
+		
+		$line = readline('Voulez vous ajouter ou voir les personnes referencer(a pour ajouter et v pour voir et q pour quitter)');
+	}
 ?>
