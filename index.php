cf54dc5f9c116193daef38fc455b3db7bf0a06c1
brice <brice.bitot@neuf.fr>
2019-01-08 09:23:48 +0100

    ajout de petit amélioration

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$line = readline('Voulez vous ajouter ou voir les personnes referencer(a pour ajouter et v pour voir et q pour quitter)');
+	$line = readline("Voulez vous ajouter ou voir les personnes referencer \n (a pour ajouter et v pour voir et q pour quitter) : ");
 	$nbpersonne = 0;
 	while ($line!= 'q') {
 		if ($line == 'a'){
@@ -12,15 +12,14 @@
 			echo "personne bien ajouter\n";
 		}
 		if ($line == 'v'){
+			$counter = 1;
 			foreach($personne as $per){
 				if ($per['age'] >= 18){
-					echo $per['nom'].' '.$per['prenom']."\n";
+					echo $counter.' - '.$per['nom'].' '.$per['prenom']."\n";
+					$counter++;
 				}
 			}
 		}
-		
-		
-		
-		$line = readline('Voulez vous ajouter ou voir les personnes referencer(a pour ajouter et v pour voir et q pour quitter)');
+		$line = readline("Voulez vous ajouter ou voir les personnes referencer \n (a pour ajouter et v pour voir et q pour quitter) : ");
 	}
 ?>
